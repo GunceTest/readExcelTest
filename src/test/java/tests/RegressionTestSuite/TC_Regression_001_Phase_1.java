9d7a8b3863ffc6f192891c4053f4378253ef3dac
GunceTest <58006298+GunceTest@users.noreply.github.com>
2020-02-17 16:57:28 +0300

    Update TC_Regression_001_Phase_1.java

--- src/test/java/tests/RegressionTestSuite/TC_Regression_001_Phase_1.java
+++ src/test/java/tests/RegressionTestSuite/TC_Regression_001_Phase_1.java
@@ -57,90 +57,16 @@ public class TC_Regression_001_Phase_1 extends TestBase {
 		
 		// *************PAGE METHODS********************
 
-//	homePage.getSetExample();
-//	tenMinutesMailPage.getSetExample();
-		
-		names.getRandomName();
-		names.getRandomLastName();
-		String name = System.getProperty("random_name_1");
-		String lastName = System.getProperty("random_last_name_1");
-		String ekipArkadasiAdi = System.getProperty("random_name_2");
-		String ekipArkadasiSoyadi = System.getProperty("random_last_name_2");
-		
-	
-		
-		
-//		tenMinutesMailPage.goToUrl(tenMinutesMail)
-//		.CreateTempMail();
-//		String userEmail = System.getProperty("user_email");
-//		loginPage.goToUrl(baseURL)
-//		.hesapOlustur(name, lastName, userEmail);
-//		tenMinutesMailPage.goToUrl(tenMinutesMail)
-//		.activateMail();
-//		loginPage.setPassword();		
+
 		loginPage.goToUrl("https://test.gunce.com");
 		loginPage.loginUser();	
-		homePage.goToMasraflar();
-		masraflarPage.failedTestMasraflariSil();
-		homePage.goToEkipArkadaslarimiz();
-		ekipArkadaslarimizPage.ekipArkadasiEkle(ekipArkadasiAdi, ekipArkadasiSoyadi);
-		homePage.goToKontaklarimiz();
-		kontaklarimizPage.kurumEkle();
-		String kurumAdi = System.getProperty("kurum_adi");
-		String adresAdi = System.getProperty("adres_adi");
-		String adres = System.getProperty("adres");
-		homePage.goToDosyalar();
-		dosyalarPage.dosyaEkle(kurumAdi, ekipArkadasiAdi, adresAdi);
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle();
-		homePage.goToRaporlar();
-		raporlarPage.raporKarsilastir();
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle2();
-		homePage.goToRaporlar();
-		raporlarPage.raporKarsilastir2();
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle3();
-		homePage.goToRaporlar();
-		raporlarPage.raporKarsilastir3();
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle4();
-		homePage.goToRaporlar();
-		raporlarPage.raporKarsilastir4();
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle5();
-		homePage.goToRaporlar();
-		raporlarPage.raporKarsilastir5();
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle6();
-		homePage.goToRaporlar();
-		raporlarPage.raporKarsilastir6();
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle7();
-		homePage.goToRaporlar();
-		raporlarPage.raporKarsilastir7();
-		homePage.goToRaporlar();
-		raporlarPage.raporFiltrele();
-		homePage.goToRaporlar();
-		raporlarPage.raporFiltrele2();
-		homePage.goToRaporlar();
-		raporlarPage.raporFiltrele3();
-		homePage.goToRaporlar();
-		raporlarPage.raporFiltrele4();
-		homePage.goToDosyalar();
-		dosyalarPage.dosyaEkle2(kurumAdi, ekipArkadasiAdi, adresAdi);
-		homePage.goToMasraflar();
-		masraflarPage.masrafEkle8();
-		homePage.goToRaporlar();
-		raporlarPage.raporFiltrele5();
-		homePage.goToRaporlar();
-		raporlarPage.raporFiltrele6();
+		
 		homePage.goToRaporlar();
 		raporlarPage.raporExcel();
 		readExcel.loadAndReadExcel();
 		raporlarPage.raporExcelCompare();
 		homePage.goToMasraflar();
-		masraflarPage.successfulTestMasraflariSil();
+		
 		//Runtime.getRuntime().exec("C:\\Users\\Gun\\Desktop\\DeleteExcelFile.exe");	
 		//jenkins icin alttaki calistirilacak
 		Runtime.getRuntime().exec("C:\\Users\\guncetest.otomasyon\\Desktop\\DeleteExcelFile.exe");
